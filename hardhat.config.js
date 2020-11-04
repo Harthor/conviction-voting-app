fe4e0c930ad9dc288c275006c816fcc086cefd36
Gaston Needleman <gastoncmr@gmail.com>
2020-11-04 07:11:24 -0300

    migrate to hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,72 @@
-require("@nomiclabs/hardhat-waffle");
+const { usePlugin } = require('@nomiclabs/hardhat/config')
+const hooks = require('./scripts/hardhat-hooks') 
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+usePlugin('@aragon/buidler-aragon') //keep an eye to migrate to hardhat
+usePlugin('@nomiclabs/hardhat-solhint')
+usePlugin('buidler-gas-reporter') // keep an eye to migrate to hardhat
+usePlugin('solidity-coverage')
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const ACCOUNTS = (process.env.ETH_KEYS ? process.env.ETH_KEYS.split(',') : [])
+  .map(key => key.trim())
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.7.3",
-};
-
+  defaultNetwork: 'localhost',
+  networks: {
+    // Local development network using ganache. You can set any of the
+    // Ganache's options. All of them are supported, with the exception
+    // of accounts.
+    // https://github.com/trufflesuite/ganache-core#options
+    ganache: {
+      url: 'http://localhost:8545',
+      gasLimit: 6000000000,
+      defaultBalanceEther: 100,
+    },
+    // Local development network to test coverage. Solidity coverage
+    // pluging launches its own in-process ganache server.
+    // and expose it at port 8555.
+    coverage: {
+      url: 'http://localhost:8555',
+    },
+    // Mainnet network configured with Aragon node.
+    mainnet: {
+      url: 'https://mainnet.eth.aragon.network',
+      accounts: ACCOUNTS,
+    },
+    // Rinkeby network configured with Aragon node.
+    rinkeby: {
+      url: 'https://rinkeby.eth.aragon.network',
+      accounts: ACCOUNTS,
+    },
+    // Network configured to interact with Frame wallet. Requires
+    // to have Frame running on your machine. Download it from:
+    // https://frame.sh
+    frame: {
+      httpHeaders: { origin: 'buidler' }, // keep an eye
+      url: 'http://localhost:1248',
+    },
+    // xDAI network configured with Aragon node.
+    xdai: {
+      url: 'https://xdai.poanetwork.dev',
+      accounts: ACCOUNTS,
+      gasPrice: 20,
+      gas: 12000000,
+    },
+  },
+  solc: {
+    version: '0.4.24',
+    optimizer: {
+      enabled: true,
+      runs: 10000,
+    },
+  },
+  gasReporter: {
+    enabled: !!process.env.GAS_REPORTER,
+  },
+  aragon: {
+    appServePort: 8001,
+    clientServePort: 3000,
+    appSrcPath: 'app/',
+    appBuildOutputPath: 'dist/',
+    hooks,
+  },
+}
